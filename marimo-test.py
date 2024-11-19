901569561266c5491ffe61a5a0c8f030e4e44d58
abdullahau <abdullah.au@outlook.com>
2024-11-20 00:22:18 +0400

    update

--- marimo-test.py
+++ marimo-test.py
@@ -7,7 +7,18 @@ app = marimo.App()
 @app.cell
 def __():
     import marimo as mo
-    return (mo,)
+    from scipy.optimize import fsolve
+    return fsolve, mo
+
+
+@app.cell
+def __(fsolve):
+    def f(x):
+        return x**2 - 6*x + 8
+
+    guess = 3
+    res = fsolve(f, guess)
+    return f, guess, res
 
 
 if __name__ == "__main__":

dfff04b36c7de0e52e6f51bf7b46f794cfd63929
abdullahau <abdullah.au@outlook.com>
2024-12-29 11:53:27 +0400

    update

--- utils/donation.py
+++ utils/donation.py
@@ -15,7 +15,8 @@ def donation_data():
                         schema={'ID': pl.Int32, **{str(i): pl.Int8 for i in year_columns}})
     )
 
-def rfm_data(data, calib_p = 6):
+def rfm_data(calib_p=6):
+    data = donation_data()
     
     calib_columns = [str(1996 + i) for i in range(calib_p)]
     valid_columns = [str(2002 + i) for i in range(len(year_columns) - calib_p)]
@@ -29,24 +30,25 @@ def rfm_data(data, calib_p = 6):
                 *[pl.when(pl.col(year) == 1).then(int(year) - 1995).otherwise(None) for year in calib_columns]
             ).alias("t_x"),
             pl.lit(calib_p).alias('np1x'), # n transaction opportunities in calibration period
-            pl.lit(len(year_columns) - calib_p).alias('np2x') # n transaction opportunities  in validation period
+            pl.lit(len(year_columns) - 1 - calib_p).alias('np2x') # n transaction opportunities  in validation period
         ).fill_null(0)
         .select('ID', 'P1X', 't_x', 'np1x', 'P2X', 'np2x')
     )
 
-def p1x_data(rfm_data):
+def p1x_data():
+    rfm_summary = rfm_data()
     return (
-        rfm_data
+        rfm_summary
         .group_by('P1X','t_x', 'np1x')
         .agg(pl.len().alias('Count'))
         .sort(['t_x', 'P1X'], descending=True)         
     )
 
-def p2x_data(rfm_data):
+def p2x_data():
+    rfm_summary = rfm_data()
     return (
-        rfm_data
+        rfm_summary
         .group_by('P2X', 'np2x')
         .agg(pl.len().alias('Count'))
         .sort(['P2X'], descending=True)         
     )
-    

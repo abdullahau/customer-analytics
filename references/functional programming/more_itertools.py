35450292b3fc0bf57cf13a18d5ae289cc1220e20
abdullahmahmood <abdullah.au@outlook.com>
2025-01-08 16:01:32 +0400

    update

--- references/functional programming/more_itertools.py
+++ references/functional programming/more_itertools.py
@@ -4,3 +4,4 @@ from more_itertools import divide
 data = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh"]
 
 [list(l) for l in divide(3, data)] #  [['first', 'second', 'third'], ['fourth', 'fifth'], ['sixth', 'seventh']] 
+
